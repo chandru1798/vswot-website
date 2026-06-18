f1f562fa4b660448f541e3404d172e95fd528c65
Chandru <chandru421998@gmail.com>
2026-06-18 14:11:48 +0530

    fix sanity studio config import

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,5 +1,10 @@
-// Change this:
-import config from '../../../sanity.config';
+import { NextStudio } from 'next-sanity/studio'
+import config from '@/sanity/config'
 
-// To this:
-import config from '../../../../sanity.config';
+export const dynamic = 'force-static'
+
+export { metadata, viewport } from 'next-sanity/studio'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
