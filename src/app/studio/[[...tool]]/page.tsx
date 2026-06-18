40a94fce932011c62e60cb8881b5b1a44cbd4094
Chandru <chandru421998@gmail.com>
2026-06-18 12:21:48 +0530

    fix: postcss and sanity path

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,5 @@
-'use client';
-
-import { NextStudio } from 'next-sanity/studio';
+// Change this:
 import config from '../../../sanity.config';
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
-}
+// To this:
+import config from '../../../../sanity.config';
